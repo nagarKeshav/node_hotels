d60f10791f7df6e96c8991041ef5a87b8d0569e9
Keshav Nagar <nagark643@gmail.com>
2025-08-25 21:36:59 +0530

    chore: add env example and update gitignore

--- db.js
+++ db.js
@@ -1,24 +1,41 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongoURL = 'mongodb://localhost:27017/Hotel';
+// Get environment variables
+const username = encodeURIComponent(process.env.MONGODB_USERNAME);
+const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+const cluster = process.env.MONGODB_CLUSTER;
+const dbname = process.env.MONGODB_DATABASE;
 
-mongoose.connect(mongoURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const mongoURL = `mongodb+srv://${username}:${password}@${cluster}/${dbname}?retryWrites=true&w=majority`;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURL, {
+      serverApi: {
+        version: '1',
+        strict: true,
+        deprecationErrors: true
+      }
+    });
+    console.log('MongoDB connected successfully!');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 const db = mongoose.connection;
 
-db.on('connected', () => {
-  console.log('MongoDB connected successfully!');
-});
-
 db.on('error', (err) => {
-  console.log('MongoDB connection error:', err);
+  console.error('MongoDB connection error:', err);
 });
 
 db.on('disconnected', () => {
   console.log('MongoDB disconnected!');
+  setTimeout(connectDB, 5000); // Retry after 5 seconds
 });
 
 module.exports = db;
